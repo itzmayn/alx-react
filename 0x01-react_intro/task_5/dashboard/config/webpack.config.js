dcb093912f99e7d5b2dccda42d03768135b7c54b
Mayn <123576452+itzmayn@users.noreply.github.com>
2024-02-01 03:55:36 +0000

    m

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,48 +1,59 @@
 const path = require('path');
 
-
+// Configuration object for webpack
 module.exports = {
-	mode: 'development',
-	entry: './src/index.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve('./dist')
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			},
-			{
-				test: /\.(png|jpe?g|gif)$/i,
-				use: [
-					'file-loader',
-					{
-						loader: 'image-webpack-loader',
-						options: {
-							bypassOnDebug: true,
-							disable: true
-						},
-					},
-				],
-			},
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env', '@babel/preset-react']
-					}
-				}
-			}
-		]
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		static: path.resolve('./dist'),
-		compress: true,
-		port: 8564,
-	},
+  // Set development mode
+  mode: 'development',
+  
+  // Entry point for bundling
+  entry: './src/index.js',
+  
+  // Output configuration
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('./dist')
+  },
+  
+  // Module rules for handling different file types
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  
+  // Generate source maps for debugging
+  devtool: 'inline-source-map',
+  
+  // Development server configuration
+  devServer: {
+    static: path.resolve('./dist'),
+    compress: true,
+    port: 8564,
+  },
 };
