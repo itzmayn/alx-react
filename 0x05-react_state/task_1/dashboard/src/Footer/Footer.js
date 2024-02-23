a301eb4af413ca3c22f3e965b2e79c0392f00595
Mayn <123576452+itzmayn@users.noreply.github.com>
2024-02-23 03:55:55 +0000

    m

--- 0x05-react_state/task_1/dashboard/src/Footer/Footer.js
+++ 0x05-react_state/task_1/dashboard/src/Footer/Footer.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { getFooterCopy, getFullYear } from '../utils/utils';
 
+/**
+ * Functional component representing the Footer of the application.
+ * @returns {JSX.Element} - React component
+ */
 function Footer() {
   return (
     <div className="App-footer">
       <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
     </div>
-  )
+  );
 }
 
 export default Footer;
